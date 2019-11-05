da8be47240a48f83d1c70f9857805f8a6c65d29b
Semen-Pyankov <sepy97@gmail.com>
2019-11-05 13:02:42 -0800

    now filter can be compiled by intel hls compiler, but only with x86-64

--- fltr.cpp
+++ fltr.cpp
@@ -1,57 +1,65 @@
-#include <stdio.h>
+#include "HLS/hls.h"
+#include <cstdio>
 #include <opencv2/opencv.hpp>
-#include <systemc>
-
 using namespace cv;
 using namespace std;
-using namespace sc_dt;
-using namespace sc_core;
 
 int kernelArray[4][3][3] = {{{-1,-1,-1},{2,2,2},{-1,-1,-1}},
                             {{-1,2,-1},{-1,2,-1},{-1,2,-1}},
                             {{-1,-1,2},{-1,2,-1},{2,-1,-1}},
                             {{2,-1,-1},{-1,2,-1},{-1,-1,2}}};
 
-SC_MODULE (GRAY)
-{
-	sc_in< uchar* > image;
-	sc_in< int > rows, cols, channels, step;
-	sc_out< uchar* > ret;
-
-	void grayscale ()
-	{
-	    uchar* g_image;
-	    int g_rows, g_cols, g_channels, g_step;
-	
-	    g_image    = image.read();
-	    g_rows     = rows.read();
- 	    g_cols     = cols.read();
-	    g_channels = channels.read();
-	    g_step     = step.read();
-
-	    uchar* grayImage = (uchar*)malloc(sizeof(uchar)*g_rows*g_cols);
-	    memset(grayImage, 0, sizeof(uchar)*g_rows*g_cols);
+extern "C" uchar* grayscale(uchar* image, int rows, int cols, int channels, int step) {
+    
+    uchar* grayImage = (uchar*)malloc(sizeof(uchar)*rows*cols);
+    memset(grayImage, 0, sizeof(uchar)*rows*cols);
         
-	    for(int y = 0; y < g_rows; y++) 
-	    {
-	        for (int x = 0; x < g_cols; x++) 
-		{
-	            int blue  = (int)g_image[g_channels*x + g_step*y];
-	            int green = (int)g_image[g_channels*x + g_step*y + 1];
-	            int red   = (int)g_image[g_channels*x + g_step*y + 2];
+    for(int y = 0; y < rows; y++) {
+        for (int x = 0; x < cols; x++) {
+            int blue = (int)image[channels*x + step*y];
+            int green = (int)image[channels*x + step*y + 1];
+            int red = (int)image[channels*x + step*y + 2];
             
-	            grayImage[x + g_cols*y] = (uchar)(.3*red) + (.59 * green) + (.11 * blue);
+            grayImage[x + cols*y] = (uchar)(.3*red) + (.59 * green) + (.11 * blue);
 
-	        }
-	    }
-	    ret.write (grayImage);
+        }
+    }
+    return grayImage;
+}
 
-	}
-
-	SC_CTOR (GRAY)
+int main(int argc, char** argv )
+{
+	if ( argc < 3 )
 	{
-		SC_METHOD (grayscale);
-		sensitive << image << rows << cols << channels << step << ret;
-	}
-};
+        	printf("usage: DisplayImage.out <Image_Path>\n");
+        	return -1;
+    	}
+	Mat image;
+	printf ("%s \n", argv[1]);
+    	image = imread( argv[1], 1 );
 
+	if (image.empty())   // !image.data )
+	{
+        	printf("No image data \n");
+        	return -1;
+    	}
+
+    	Mat processedImage;
+
+	if (strcmp(argv[2], "-g") == 0) 
+	{
+        	unsigned char* grayImageData = grayscale(image.data, image.rows, image.cols, image.channels(), image.step);
+        	processedImage = Mat(image.rows, image.cols, CV_8UC1, grayImageData);
+	}
+	
+	//namedWindow("Display Image", WINDOW_AUTOSIZE );
+
+    	//imshow("Display Image", processedImage);
+    	//waitKey(0);
+
+	imwrite (strcat(argv[2], argv[1]), processedImage);
+	printf ("%s \n", strcat(argv[2], argv[1]));
+	
+	return 0;
+
+}
