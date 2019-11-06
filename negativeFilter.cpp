7ccae08000417b21d29987b374faca32fbbc624b
taylorfutral <taylorfutral@MacBook-Pro.local>
2019-11-06 11:57:28 -0800

    added negative and segmentation filter

---
+++ negativeFilter.cpp
@@ -0,0 +1,43 @@
+// modified from https://github.com/iamrachitgulati/image-filter
+
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+  //Load an Image
+
+  if ( argc < 3 )
+  {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+  }
+  IplImage* image;
+  image = cvLoadImage( argv[1]);
+
+  if (image.empty())   // !image.data )
+  {
+          printf("No image data \n");
+          return -1;
+  }
+
+  cv::namedWindow("Image", CV_WINDOW_AUTOSIZE);
+  cvShowImage("Image", img);
+
+  //Negative Effect
+  cvNot(img, img);
+  cv::namedWindow("NegativeEffect", CV_WINDOW_AUTOSIZE);
+  cvShowImage("NegativeEffect", img);
+
+  //Wait Key press
+  cvWaitKey(0);
+
+  //destroy
+  cvDestroyWindow("Image");
+  cvDestroyWindow("NegativeEffect");
+
+  return 0;
+}
